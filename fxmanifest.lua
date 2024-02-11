5620b4c1ac98306615fbadb568ba654fe31de781
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-02-11 10:43:49 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,19 @@
-fx_version "adamant"
-games { "rdr3" }
-author 'VORP BlackPegasus'
-rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+fx_version 'cerulean'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+
+author 'VORP @blackPegasus'
+description 'A tool to view imaps in your server for vorp core framework'
+repository 'https://github.com/VORPCORE/vorp_imapviewtool'
 
 client_scripts {
     "imaps_with_coords_and_heading.lua",
     "client.lua"
 }
+
+--dont touch
+version '1.0'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/VORPCORE/vorp_imapviewtool'
